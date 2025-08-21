22af028e53130017683f842fc48e994d426d557e
noboKumar <nobokumar227@gmail.com>
2025-08-21 23:22:55 +0600

    add products and dashboard route page

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <div>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
