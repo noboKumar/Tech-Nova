04ca9ffc6fc86a8561379132d9e69884cf1c0f04
noboKumar <nobokumar227@gmail.com>
2025-08-21 21:50:50 +0600

    update nav links and add active class

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 import React from "react";
 import icon from "../../public/techNova-icon.png";
+import Link from "next/link";
 
 const Logo = () => {
   return (
-    <div className="flex items-center gap-2">
-      <Image width={50} height={50} src={icon} alt="logo" />
-      <h1 className="text-2xl font-bold">TechNova</h1>
-    </div>
+    <Link href="/">
+      <div className="flex items-center gap-1">
+        <Image width={50} height={50} src={icon} alt="logo" />
+        <h1 className="text-2xl font-bold">
+          <span className="text-blue-700">Tech</span>
+          <span className="text-amber-600">Nova</span>
+        </h1>
+      </div>
+    </Link>
   );
 };
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,17 @@
+"use client";
 import React from "react";
 import Logo from "./Logo";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
-  const navLinks = (
-    <>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-    </>
-  );
+  const navLinks = [
+    { name: "Home", href: "/" },
+    { name: "Products", href: "/products" },
+    { name: "Dashboard", href: "/dashboard" },
+  ];
+  const pathName = usePathname();
   return (
     <div className="bg-gray-200 border-b-2 border-gray-300 sticky top-0 z-10">
       <div className="flex items-center justify-between p-4 w-11/12 mx-auto">
@@ -19,8 +20,23 @@ const NavBar = () => {
           <Logo />
         </div>
         {/* nav Links */}
-        <div>
-          <ul>{navLinks}</ul>
+        <div className="flex items-center gap-5">
+          {navLinks.map((link) => {
+            const isActive =
+              pathName === link.href ||
+              (pathName.startsWith(link.href) && link.href !== "/");
+            return (
+              <Link
+                href={link.href}
+                key={link.name}
+                className={`${
+                  isActive ? "bg-gray-300" : ""
+                } text-lg hidden md:flex gap-5 font-semibold px-4 py-2 rounded-xl`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
         </div>
         {/* nav end */}
         <div>
