adb51029e01f75dfa5cc94c91b96acbd5399275f
noboKumar <nobokumar227@gmail.com>
2025-08-22 18:51:57 +0600

    fix create user to login

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,24 +1,42 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
 import Link from "next/link";
 import { useState } from "react";
 
 export default function RegisterPage() {
   const [form, setForm] = useState({ name: "", email: "", password: "" });
   const [msg, setMsg] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function onSubmit(e) {
     e.preventDefault();
     setMsg("");
+    setLoading(true);
     const res = await fetch("/api/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form),
     });
     const data = await res.json();
-    if (res.ok) setMsg("Account created. You can log in now.");
-    else setMsg(data.error || "Error");
+
+    if (res.ok) {
+      const loginRes = await signIn("credentials", {
+        redirect: true,
+        email: form.email,
+        password: form.password,
+        callbackUrl: "/products",
+      });
+
+      if (loginRes?.error) {
+        setMsg("Registered but auto-login failed. Please login manually.");
+      }
+    } else {
+      setMsg(data.error || "Error");
+    }
+
+    setLoading(false);
   }
 
   return (
@@ -49,12 +67,14 @@ export default function RegisterPage() {
           className="w-full border p-2 font-semibold rounded-2xl cursor-pointer"
           type="submit"
         >
-          Sign up
+          {loading ? "Creating..." : "Sign up"}
         </Button>
         <p className="font-semibold text-gray-700">
           Already have an account?{" "}
           <span>
-            <Link className="underline" href={"/login"}>Login</Link>
+            <Link className="underline" href={"/login"}>
+              Login
+            </Link>
           </span>
         </p>
       </form>
