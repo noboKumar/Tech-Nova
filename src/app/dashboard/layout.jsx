9fc7aea9b655ff0ecad717ed7c2551ca7cdc19ef
noboKumar <nobokumar227@gmail.com>
2025-08-22 18:44:54 +0600

    protech dashboard unauthenticated user

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,7 +1,14 @@
 import Sidebar from "@/components/SideBar";
+import { getServerSession } from "next-auth";
 import React from "react";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-const DashboardLayout = ({ children }) => {
+const DashboardLayout = async ({ children }) => {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/login");
+  }
   return (
     <div className="flex">
       <Sidebar></Sidebar>
